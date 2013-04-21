a9c5c6e30825421f17d116c7c6025d1a7fb4a1c3
Viscatus <viscatus@gmail.com>
2013-04-21 15:44:07 +0300

    1.0.03

---
+++ user_spec.rb
@@ -0,0 +1,14 @@
+require "rspec"
+require_relative "user"
+
+describe "new" do
+  before :each do
+    @user = User.new
+  end
+
+  it "should create itself succesfully" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
